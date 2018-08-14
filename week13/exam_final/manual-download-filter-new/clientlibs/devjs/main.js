9101b2791c95a0f8e641677c0e33d36346fb22bd
mkfdev <rosy.fdev@gmail.com>
2018-08-14 19:04:13 +0900

    js 4줄 커밋

--- week13/exam_final/manual-download-filter-new/clientlibs/devjs/main.js
+++ week13/exam_final/manual-download-filter-new/clientlibs/devjs/main.js
@@ -1,5 +1,9 @@
 (function (win, $, doc) {
     'use strict';
+    win.smg = win.smg || {};
+    win.smg.support = win.smg.support || {};
+    win.smg.support.common = win.smg.support.common || {};
     
+    var 
         
 })(window, window.jQuery, window.document)
